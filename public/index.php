bb2b312030486c5190fb9cfd29a3e622f95124b0
mattisbbn <mattisbbn@gmail.com>
2025-03-27 22:01:25 +0100

    Update build

--- public/index.php
+++ public/index.php
@@ -5,6 +5,17 @@ $dotenv->load();
 require_once "../src/Router/Router.php";
 
 if (isset($_SERVER['VITE_DEV_SERVER'])) {
+    // Mode développement
     echo '<script type="module" src="' . $_SERVER['VITE_DEV_SERVER'] . '/resources/js/main.js"></script>';
-} 
+} else {
+    // Mode production : charger les fichiers buildés
+    $manifestPath = __DIR__ . '/../public/build/manifest.json';
+    if (file_exists($manifestPath)) {
+        $manifest = json_decode(file_get_contents($manifestPath), true);
+        if (isset($manifest['resources/js/main.js'])) {
+            $filePath = "/build/" . $manifest['resources/js/main.js']['file'];
+            echo '<script type="module" src="' . $filePath . '"></script>';
+        }
+    }
+}
 ?>
