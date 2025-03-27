fc5ec8d015b41b31dcd5c64a777e2f898950b7de
mattisbbn <mattisbbn@gmail.com>
2025-03-27 22:09:51 +0100

    Test build

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 require_once "../src/Router/Router.php";
 
-if (isset($_SERVER['VITE_DEV_SERVER'])) {
+if ($_ENV["ENVIRONNEMENT"] === "dev") {
     // Mode développement
     echo '<script type="module" src="' . $_SERVER['VITE_DEV_SERVER'] . '/resources/js/main.js"></script>';
 } else {
     // Mode production : charger les fichiers buildés
-    $manifestPath = __DIR__ . '/../public/build/manifest.json';
+    $manifestPath = __DIR__ . '/.vite/manifest.json';
     if (file_exists($manifestPath)) {
         $manifest = json_decode(file_get_contents($manifestPath), true);
         if (isset($manifest['resources/js/main.js'])) {
