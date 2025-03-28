8985dfbab064772a177d1a4552a6410266a0f8d9
Mattisbbn <mattisbbn@gmail.com>
2025-03-28 15:39:36 +0100

    update

--- src/Views/Landpage/Sections/Introduction.php
+++ src/Views/Landpage/Sections/Introduction.php
@@ -1,4 +1,4 @@
-<section id="introduction" class="flex h-dvh flex-col align-middle justify-center md:w-5/10 ms-auto me-auto ">
+<section id="introduction" class="flex h-dvh flex-col align-middle justify-center md:w-6/10 lg:w-5/10 ms-auto me-auto ">
    <h1 class="text-center text-4xl md:text-6xl font-bold mb-6">Introduction</h1>
    <p class="text-3xl font-semibold">Je m'appelle Mattis, j'ai 19 ans et je suis actuellement en première année de <a class="text-blue-marguerite-600 font-bold" target="_blank" href="https://www.onisep.fr/ressources/univers-formation/formations/post-bac/bts-services-informatiques-aux-organisations-option-b-solutions-logicielles-et-applications-metiers">BTS SIO option SLAM</a> à <a href="https://www.mydigitalschool.com/ecole-multimedia-angers" class="text-blue-marguerite-600 font-bold">MyDigitalSchool</a> . J'aime toucher à tout et être autonome sur les projets que l'on me confie.</p>
 </section>
--- src/Views/Partials/Head.php
+++ src/Views/Partials/Head.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="<?php echo $_ENV["WEBSITE_LOCALE"] ?>">
 <head>
-    <title>Portfolio Mattis Babin | Développeur web</title>
+    <title>Mattis Babin | Développeur web</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="title" content="Portfolio Mattis Babin | Développeur web" />
+    <meta name="title" content="Mattis Babin | Développeur web" />
     <meta name="description" content="Porfolio de Mattis Babin. Développeur Web PHP/JS en alternance chez Akoufen." />
     <link rel="icon" href="/img/global/logo.ico">
 
@@ -26,10 +26,8 @@
     <title><?php echo $_ENV["WEBSITE_NAME"] ?></title>
 
     <?php if ($_ENV["ENVIRONNEMENT"] === "DEV") {
-    // Mode développement
     echo '<script type="module" src="' . $_SERVER['VITE_DEV_SERVER'] . '/resources/js/main.js"></script>';
 } else {
-    // Mode production : charger les fichiers buildés
     $manifestPath = '.vite/manifest.json';
     if (file_exists($manifestPath)) {
         $manifest = json_decode(file_get_contents($manifestPath), true);
