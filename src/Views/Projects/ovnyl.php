4b2265983223da3d3459c55c5485b3e22b085ee5
mattisbbn <mattisbbn@gmail.com>
2025-04-29 21:51:03 +0200

    upd

--- src/Views/Projects/ovnyl.php
+++ src/Views/Projects/ovnyl.php
@@ -1,4 +1,54 @@
-<section class="mt-6">
+<section class="mt-6 max-w-5/10 mx-auto mb-5">
     <h1 class="text-center md:text-4xl text-2xl font-semibold mb-10 ">Projet : Ovnyl</h1>
     <img class="m-auto w-[50vw] rounded-xl" src="/img/projets/ovnyl-unblur.jpg" alt="Illustration Ovnyl">
+
+
+
+    <h2 class="text-center mt-10 md:text-4xl text-2xl font-semibold mb-6">Contexte</h2>
+    <p class="text-lg text-gray-600 font-medium">Ce projet a été réalisé dans le cadre de mon alternance chez Akoufen, il s’agissait ici d’intervenir pour un client de l’entreprise, souhaitant moderniser son site internet, Ovnyl, dédié à la vente de vinyles personnalisés.
+    <br><br>
+    Le projet consistait à effectuer une refonte complète du site, tant sur le plan technique que graphique. L’objectif était de le rendre plus moderne, performant et en phase avec les standards actuels du web. Cela impliquait :</p>
+    <br>
+
+    <ul class="text-lg  ms-6 list-disc list-inside text-gray-600 font-medium">
+        <li>L'ajout de nouvelles fonctionnalités</li>
+        <li>L'intégration d'une nouvelle charte graphique fournie via une maquette Figma</li>
+        <li>La mise à jour de la version de PHP</li>
+        <li>La mise à jour des dépendances du projet</li>
+    </ul>
+<br>
+    <p class="text-lg text-gray-600 font-medium">Ce projet m’a permis de travailler dans un contexte professionnel réel, en respectant un cahier des charges client, des délais, et en échangeant régulièrement avec le client et les membres de l’équipe d’Akoufen.</p>
+   
+
+    <h2 class="text-center mt-10 md:text-4xl text-2xl font-semibold mb-6">Objectifs</h2>
+
+    <ul class="text-lg  ms-6 list-disc list-inside text-gray-600 font-medium">
+        <li>Ajouter de nouvelles fonctionnalités pour répondre aux besoins actuels du client et améliorer l’interaction des utilisateurs avec le site.</li>
+        <br>
+        <li>Moderniser l’image du site Ovnyl en appliquant une nouvelle charte graphique, conforme à la maquette Figma fournie.</li>
+        <br>
+        <li>Mettre à jour l’environnement technique du site afin d’assurer sa compatibilité avec les versions récentes de PHP et des dépendances utilisées.</li>
+        <br>
+        <li>Améliorer l’expérience utilisateur sur tous types d’appareils en proposant une interface responsive, fluide et intuitive.</li>
+        <br>
+        <li>Renforcer la stabilité et la maintenabilité du site à travers un code plus propre, mieux structuré, et basé sur des technologies à jour.</li>
+    </ul>
+
+
+    <h2 class="text-center mt-10 md:text-4xl text-2xl font-semibold mb-6">Technologies utilisées</h2>
+
+   <div class="flex justify-center gap-1.5">
+        <img class="h-14" src="/img/Icons/PHP.svg" alt="PHP">
+        <img class="h-14" src="/img/Icons/JS.svg" alt="JS">
+        <img class="h-14" src="/img/Icons/Bootstrap.svg" alt="Bootstrap">
+        <img class="h-14" src="/img/Icons/paypal.svg" alt="Paypal">
+   </div>
+
+   <h2 class="text-center mt-10 md:text-4xl text-2xl font-semibold mb-6">Lien du projet</h2>
+   <div class="flex justify-center gap-6">
+   <a  disabled target="_blank" class="text-white bg-gray-400 p-2 border-2 rounded-lg cursor-pointer border-gray-400 hover:bg-gray-400">Site actuel (Bientôt en ligne)</a>
+   <a href="http://pdvyqfz.cluster027.hosting.ovh.net/" target="_blank" class="text-white bg-blue-marguerite-600 p-2 border-2 rounded-lg cursor-pointer border-blue-marguerite-600 hover:bg-blue-marguerite-600">Version de developpement</a>
+   </div>
+   
+   
 </section>
