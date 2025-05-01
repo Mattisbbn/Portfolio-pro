9ee35947982ec65e68c07a10922e03bc78ee217e
mattisbbn <mattisbbn@gmail.com>
2025-05-01 18:30:10 +0200

    update

--- src/Views/404/404.php
+++ src/Views/404/404.php
@@ -1,3 +1,8 @@
+<?php 
+http_response_code(404);
+
+?>
+
 <section class="flex h-[100dvh] bg-gray-200 ">
     <div class="m-auto flex align-middle justify-center flex-col">
         <h1 class="text-center md:text-6xl text-3xl  font-semibold">Erreur 404</h1>
